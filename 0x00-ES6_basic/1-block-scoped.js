60b42260c3a997b051fb90e4d311f5b8ca670fcc
codemidev <ochaijay81@gmail.com>
2023-04-19 11:24:26 +0100

    Hotfix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,29 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-<<<<<<< HEAD
   const task = false;
   const task2 = true;
 
+  /* eslint-disable no-unused-vars */
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
   }
+  /* eslint-enable no-unused-vars */
 
   return [task, task2];
-=======
-
-  const task = false;
-
-  const task2 = true;
-
-  if (trueOrFalse) {
-
-    const task = true;
-
-    const task2 = false;
-
-  }
-
-  return [task, task2];
-
->>>>>>> 2f425487bbb18f0cdcd33ed393af2c5223e2a609
 }
